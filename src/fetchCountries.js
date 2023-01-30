48c851bdfaaab2455476672c4d6359a6412c2d3a
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-01-30 19:39:22 +0200

    addfetch

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+
+const url = "https://restcountries.com/v3.1/name";
+
+
+ function fetchCountries(name) {
+    
+  return fetch(`${url}/${name}?fields=name,capital,population,flags,languages`)
+  .then(res => {
+    return res.json();})
+    }
+
+    export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,82 @@
+
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import fetchCountries  from './fetchCountries.js'
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.getElementById('search-box');
+const listEl = document.querySelector('.country-list')
+const infoEl = document.querySelector('.country-info')
+
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+    let  inputValue = e.target.value.trim();
+  if (!inputValue) {
+    resetCard(listEl);
+    resetCard(infoEl);
+    return;
+  }
+
+  fetchCountries(inputValue).then(data =>  {
+    
+    if (data.length > 10) { 
+      Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  resetCard(listEl);
+  resetCard(infoEl);
+    return;
+  }
+  
+  createMarkup(data);
+     
+  }).catch(err => {
+    resetCard(listEl);
+    resetCard(infoEl);
+  
+    Notiflix.Notify.failure("Oops, there is no country with that name");
+  });
+  }
+  
+  
+  function resetCard(card) {
+    card.innerHTML = "";
+  }
+  
+  function markupCountryList(countries)  {
+  return countries.map(
+  
+    ({ name, flags }) => 
+    `<li class = list> 
+    <img class = img_list src = "${flags.svg}" alt = "${name.official}" >
+    ${name.official}
+    </li>`
+  ).join("");
+  
+  }
+  
+  function markupCountryInfo(country)  {
+    return country.map(({ name, capital, population, flags, languages }) => {
+      return `
+          <h1 class="name"><img class=img src="${flags.svg} " alt="${
+        name.official
+      }" ${name.official}</h1>
+          <p class="text">Capital: ${capital[0]}</p>
+          <p class="text">Population: ${population}</p>
+          <p class="text">Languages: ${Object.values(languages).join(',')}</p>
+        `;
+    });
+  }
+  
+  function createMarkup(countries) {
+    if (countries.length === 1) {
+      resetCard(listEl);
+      infoEl.innerHTML = markupCountryInfo(countries);
+    }
+    else {
+    resetCard(infoEl);
+    listEl.innerHTML = markupCountryList(countries);
+    }
+  }
